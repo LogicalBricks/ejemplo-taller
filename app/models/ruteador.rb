c138023ee12bae55ee114f4351211839aad3d7b0
moiseserg <moiseserg@gmail.com>
2013-10-05 05:41:31 -0500

    Agregando validaciones al Ruteador de  usuario y observaciones

--- app/models/ruteador.rb
+++ app/models/ruteador.rb
@@ -3,6 +3,6 @@ class Ruteador < ActiveRecord::Base
 	validates_presence_of :password
 	validates_presence_of :puerto
 	validates_numericality_of :puerto
-
-	
+	validates_presence_of :usuario
+	validates_presence_of :observaciones
 end
--- spec/models/ruteador_spec.rb
+++ spec/models/ruteador_spec.rb
@@ -5,4 +5,7 @@ describe Ruteador do
   it{ should validate_presence_of(:password) }
   it{ should validate_presence_of(:puerto) }
   it{ should validate_numericality_of(:puerto) }
+  it{ should validate_presence_of(:usuario) }
+
+  it{ should validate_presence_of(:observaciones) }
 end
