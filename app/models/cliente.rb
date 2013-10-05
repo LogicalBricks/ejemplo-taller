19fd40ff9c61696cba00c14b18fb730c53884e1f
CLu1s <che.luis01@gmail.com>
2013-10-05 12:55:20 -0500

    agregadas pruebas y validaciones para modelo cliente

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,3 +1,8 @@
 class Cliente < ActiveRecord::Base
 	validates :rfc, presence: true
+	validates :razon_social, presence: true
+	validates :direccion, presence: true
+	validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/,
+    message: "Only letters allowed" }
+    validates :nombre_contacto, presence: true
 end
